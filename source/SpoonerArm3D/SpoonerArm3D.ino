1a13393560cb7436e248822dc2cf28108f673c84
Juan C.Cilleruelo Gonzalo <juanc.cilleruelo@gmail.com>
2022-06-30 14:30:35 +0200

    Probes in the ino source and new documentation.
    Probes about the positioning slowly of the branches.
    It is not possible, at less as we know, to move the

--- source/SpoonerArm3D/SpoonerArm3D.ino
+++ source/SpoonerArm3D/SpoonerArm3D.ino
@@ -1,36 +1,56 @@
 #include <Wire.h>
 //#include <PWMServo.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <Adafruit_PWMServoDriver.h>  //PCA9685 servo motor driver.
 
 Adafruit_PWMServoDriver myServo = Adafruit_PWMServoDriver();
 
-#define SERVOMIN 150
-#define SERVOMAX 600
+//These values are dependant of the servo manufacurer
+#define SERVOMIN 150  //  0º position
+#define SERVOMAX 600  //180º position
 
-uint8_t servonum       = 0;
-uint8_t numberOfServos = 2;
+uint8_t CurServo       = 0; //Current Servo
+uint8_t numberOfServos = 3;
 
 void setup() {
    Serial.begin(9600);
    myServo.begin();
+
+   //The frequency is set to 60Hz which is equivalent to a PWM signal with 16.6ms period 
+   //that is within the duty cycle range of most servos.
    myServo.setPWMFreq(60);
+
    delay(10);
+
+   myServo.setPWM(0, 0, pulselen);
+
+   myServo.setPWM(1, 0, pulselen);
 }
 
 void loop() {
-   for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++){
-      myServo.setPWM(servonum, 0, pulselen);
-   }
 
+  //The function setPWM(n_servo, on, off) is used to set the pulse width of a PWM output.
+  //  n_servo :the number of servo or output to be configured (0 to 15)
+  //  on      :the value the PWM signal rising edge, that is, when the signal should transition from low to high. 
+  //           Since it is 12-bit signal, the value should be between 0 to 4095.
+  //  off     :the value of PWM signal falling edge, that is, when the signal should transition 
+  //           from high to low and the value ​​​​must also be between 0 and 4096.
+
+   for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++){
+      myServo.setPWM(2, 0, pulselen);
+      //delay(5);
+   }
    delay(500);
+
    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--){
-      myServo.setPWM(servonum, 0, pulselen);
+      myServo.setPWM(2, 0, pulselen);
+      delay(2);
    }
    delay(500);
   
-   servonum++;
-   if (servonum > numberOfServos-1) 
-       servonum = 0; 
+   CurServo++;
+   if (CurServo > numberOfServos -1) {
+      CurServo = 0; 
+   }   
 
 }
 
